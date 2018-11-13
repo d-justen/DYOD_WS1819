23b360c123db002f4b5ebee2c3ad8269e67ad278
David Justen <mail@davidjusten.de>
2018-11-13 18:57:57 +0100

    added fitted attribute vector tests

---
+++ src/test/storage/fitted_attribute_vector_test.cpp
@@ -0,0 +1,37 @@
+#include <memory>
+#include <string>
+
+#include "gtest/gtest.h"
+
+#include "../../lib/storage/fitted_attribute_vector.hpp"
+
+namespace opossum {
+
+class StorageFittedAttributeVectorTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+  fav->set(0, ValueID{3});
+  fav->set(1, ValueID{1});
+  fav->set(2, ValueID{0});
+  fav->set(3, ValueID{2});
+  }
+
+  std::shared_ptr<opossum::FittedAttributeVector<uint8_t>> fav = std::make_shared<opossum::FittedAttributeVector<std::uint8_t>>(4);
+};
+
+TEST_F(StorageFittedAttributeVectorTest, GetSet) {
+    EXPECT_EQ(fav->get(0), ValueID{3});
+    EXPECT_EQ(fav->get(1), ValueID{1});
+    EXPECT_EQ(fav->get(2), ValueID{0});
+    EXPECT_EQ(fav->get(3), ValueID{2});
+}
+
+TEST_F(StorageFittedAttributeVectorTest, Size) {
+    EXPECT_EQ(fav->size(), 4ul);
+}
+
+TEST_F(StorageFittedAttributeVectorTest, Width) {
+    EXPECT_EQ(fav->width(), 1u);
+}
+
+} // namespace opossum
